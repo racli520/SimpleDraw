8a36ce0ce5b51dc30dcf970bd0c4140ff0639587
Rachel Li <Rachel@Rachel-Lis-MacBook-Pro.local>
2017-02-19 22:03:02 -0800

    Initial Commit

---
+++ SimpleDraw/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleDraw
+//
+//  Created by Rachel Li on 2/19/17.
+//  Copyright © 2017 Rachel Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
